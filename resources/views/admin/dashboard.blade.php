d21ac8250fb1cf0627f9b55c23d74a10aaa0cf75
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2026-02-20 20:11:43 +0100

    first commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends("admin.adminLayouts.sidebarlayout")
+
+@section("content")
+
+<h1>SONO IL CONTENUTO DELLA DASHBOARD</h1>
+
+@endsection
---
+++ resources/views/admin/profile.blade.php
@@ -0,0 +1,7 @@
+@extends("admin.adminLayouts.sidebarlayout")
+
+@section("content")
+
+<h1>SONO IL CONTENUTO DELLA PROFILE PAGE</h1>
+
+@endsection
